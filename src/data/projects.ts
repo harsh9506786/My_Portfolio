b4fe806f5e763f04a62856194db98143b77d32bc
Harshvardhan Sharma <sharmaharshvardhan2805@gmail.com>
2026-08-02 14:18:53 +0530

    story-app apk link update-I

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -134,7 +134,8 @@ Nginx Reverse Proxy (Docker, ports 80/443)
       "Reanimated",
       "NativeWind",
     ],
-    live: "https://expo.dev/artifacts/eas/pNxq3aFQX-7g3TJMQXe9ZNmMoadkZHYTBkTVUDCGLUw.apk",
+    live: "https://expo.dev/artifacts/eas/6qgDqnyrAJNqCK3SQaOfRPcKIjZ2DgjIn4rzldS-iMs.apk"
+    , 
     github: "https://github.com/harsh9506786/Story-App",
     caseStudy: [
       {
